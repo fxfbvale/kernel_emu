cd68cc33561b5406c22bec3415206deea0ad0811
fxfbvale <fxvale@campus.fct.unl.pt>
2024-07-07 17:59:45 +0200

    final changes to modules

--- src/bof_module.c
+++ src/bof_module.c
@@ -8,28 +8,38 @@
 
 
 MODULE_LICENSE("GPL"); 
+MODULE_AUTHOR("vale");
+MODULE_DESCRIPTION("A kernel module with a buffer overflow vulnerability.");
 
-char leak[128];
+char message_receiver[0x100];
+
 static int device_open(struct inode *inode, struct file *filp)
 {
-    	printk(KERN_ALERT "Device opened.\n");
+    	printk(KERN_DEFAULT "Device opened.\n");
   	return 0;
 }
 
 static int device_release(struct inode *inode, struct file *filp)
 {
-    	printk(KERN_ALERT "Device closed.\n");
+    	printk(KERN_DEFAULT "Device closed.\n");
   	return 0;
 }
 
 
 static ssize_t device_write(struct file *filp, const char *buf, size_t len, loff_t *off) {
-    char buffer[100];
+    int ret;
+    char tmp_buffer[100];
 
-    printk(KERN_INFO "Testing...");
-    memcpy(buffer, buf, 0x100);
+    printk(KERN_ALERT "Testing...");
+    ret = copy_from_user(message_receiver, buf, len);
 
-    printk(KERN_INFO "Yes? %s", buffer);
+    if (ret)
+		  return -EFAULT;
+
+    memcpy(tmp_buffer, message_receiver, strlen(message_receiver));
+
+
+    printk(KERN_ALERT "Yes? %s", tmp_buffer);
 
     return 0;
 }
@@ -45,13 +55,13 @@ struct proc_dir_entry *proc_entry = NULL;
 int init_module(void)
 {
   proc_entry = proc_create("bof", 0666, NULL, &fops);
-  printk(KERN_ALERT "/proc/bof created\n");
+  printk(KERN_DEFAULT "/proc/bof created\n");
   return 0;
 }
 
 void cleanup_module(void)
 {
 	if (proc_entry) proc_remove(proc_entry);
-    printk(KERN_ALERT "/proc/bof reomved\n");
+    printk(KERN_DEFAULT "/proc/bof reomved\n");
 
 }
--- src/proc_leak.c
+++ src/proc_leak.c
@@ -6,31 +6,41 @@
 #include <linux/fs.h>
 #include <linux/ioctl.h>
 
+#define LEAK_SIZE 21
 
 MODULE_LICENSE("GPL"); 
+MODULE_AUTHOR("vale");
+MODULE_DESCRIPTION("A simple kernel module that leaks the address of kernel_listen.");
 
 char leak[128];
+
 static int device_open(struct inode *inode, struct file *filp)
 {
-    	printk(KERN_ALERT "Device opened.\n");
+    	printk(KERN_DEFAULT "Device opened.\n");
   	return 0;
 }
 
 static int device_release(struct inode *inode, struct file *filp)
 {
-    	printk(KERN_ALERT "Device closed.\n");
+    	printk(KERN_DEFAULT "Device closed.\n");
   	return 0;
 }
 
 void read_proc_kallsyms(void)
 {
-  // read in the /proc/kallsyms file
-  loff_t offset = 0;
+  /**
+   * read in the /proc/kallsyms file
+   
+   - Number of bytes until kernel_listen
+   * head -n 39682 /proc/kallsyms | wc -c
+      1588987
+  */
+  loff_t offset = 1588987;
   struct file *leak_fd;
   leak_fd = filp_open("/proc/kallsyms", O_RDONLY, 0);
-  kernel_read(leak_fd, leak, 128, &offset);
+  kernel_read(leak_fd, leak, 16, &offset);
   filp_close(leak_fd, NULL);
-  printk(KERN_ALERT "leak: %s\n", leak);
+  printk(KERN_DEFAULT "leak: %s\n", leak);
 
 }
 
@@ -39,8 +49,14 @@ static long proc_read(struct file *filp, char *buffer, size_t length, loff_t *of
 {
   read_proc_kallsyms();
 
-	int ret = 0;	
-  ret = copy_to_user(buffer, leak, 128);
+	int ret = 0;
+  char leak_str[LEAK_SIZE];
+  unsigned long leak_ul;
+  kstrtoul(leak, 16, &leak_ul);
+  snprintf(leak_str, LEAK_SIZE, "%lu", leak_ul);
+  ret = copy_to_user(buffer, leak_str, LEAK_SIZE);
+
+  printk(KERN_DEFAULT "leak_to_ul: %s\n", leak_str);
 
 	if (ret)
 		return -EFAULT;
@@ -58,12 +74,12 @@ struct proc_dir_entry *proc_entry = NULL;
 int init_module(void)
 {
   proc_entry = proc_create("leak", 0666, NULL, &fops);
-  printk(KERN_ALERT "/proc/leak created\n");
+  printk(KERN_DEFAULT "/proc/leak created\n");
   return 0;
 }
 
 void cleanup_module(void)
 {
 	if (proc_entry) proc_remove(proc_entry);
-  printk(KERN_ALERT "/proc/leak reomved\n");
+  printk(KERN_DEFAULT "/proc/leak reomved\n");
 }
